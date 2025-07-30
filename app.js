b3a34b562e9905d1e6a0a345b9d237b17cd1d1e8
Yessica Arias <yessica.sandrith@gmail.com>
2025-07-30 16:15:17 -0500

    Final del Proyecto Amigo Secreto

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaArrayAmigos = [];
+
+function agregarAmigo () {
+    let nombresDeAmigos = document.getElementById('amigo').value;
+    if (nombresDeAmigos === '') {
+        alert('¡Error!, Porfavor Inserte un Nombre.');
+    } else {
+        listaArrayAmigos.push(nombresDeAmigos);
+        agregarNombres();
+    }
+  document.getElementById('amigo').value = ''; 
+  console.log(listaArrayAmigos) 
+} 
+
+function agregarNombres () {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+    for (let i = 0; i < listaArrayAmigos.length; i++) {
+        let item = document.createElement('li');
+        item.textContent = listaArrayAmigos[i];
+        listaHTML.appendChild(item);
+    }
+    console.log(listaHTML)
+}
+
+function sortearAmigo () {
+    if (listaArrayAmigos.length === 0) {
+        alert('No has Ingresado Nombres')
+        return;
+    } 
+
+    let indiceAleatorio = Math.floor(Math.random() * listaArrayAmigos.length);
+    let amigoSorteado = listaArrayAmigos[indiceAleatorio];    
+    let contenedorResultado = document.getElementById('resultado');
+    contenedorResultado.innerHTML = `${amigoSorteado}, fue el amigo secreto seleccionado 🎉`;
+
+}
